20f2fa8b5f254bc8e4f5ccdc32f0f66c1a1c066f
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2021-06-13 16:00:29 +0200

    finally got the functionality working. next to tackle is responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,13 @@ faqList.forEach(faq => {
 
 // FUNCTION DECLARATIONS
 function toggleFaq(e) {
-     if(e.target.className == "faq-header" || e.target.className == "") {
-          e.target.classList.toggle("active");
-     }
+     // FIRST WE LOOP THROUGH faqList AND IF THERE IS A faq THAT IS EXPANDED, COLLAPSE IT.
+     faqList.forEach(faq => {
+          // IF THE faq HAS A CLASS OF active, REMOVE IT
+          if(faq.classList.contains("active")) faq.classList.remove("active");
+     });
+
+     // THE this KEYWORD REFERES TO THE CURRENT ITEM THAT WAS CLICKED 
+     // ONCE ALL THE OTHER active CLASSES HAVE BEEN REMOVED, ADD IT TO THE CURRENT faq
+     this.classList.toggle("active");
 }
